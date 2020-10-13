d6a820c51fa13b0584522aed8d4c1066dc5d865e
Karter13 <KarMiH1975@mail.ru>
2020-10-13 15:30:03 +0300

    created application architecture

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -6,7 +6,12 @@ type AppPropsType = {}
 export const App: React.FC<AppPropsType> = (props) => {
     return (
         <div className="App">
-
+            {/*hr.prov*/}
+            <div>
+                <h1>REACT</h1>
+                {/*<Header/>*/}
+                {/*<Main/>*/}
+            </div>
         </div>
     );
 };
