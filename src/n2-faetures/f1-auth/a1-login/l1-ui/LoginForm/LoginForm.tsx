dea010638d1fa0f2fe72f1449c0afd61b08bcd55
Karter13 <KarMiH1975@mail.ru>
2020-10-17 18:17:53 +0300

    made and checked UI for LoginPage

--- src/n2-faetures/f1-auth/a1-login/l1-ui/LoginForm/LoginForm.tsx
+++ src/n2-faetures/f1-auth/a1-login/l1-ui/LoginForm/LoginForm.tsx
@@ -3,22 +3,29 @@ import {Input} from '../../../../../n1-main/m1-ui/common/Input/Input';
 import {Button} from '../../../../../n1-main/m1-ui/common/Button/Button';
 import {Checkbox} from '../../../../../n1-main/m1-ui/common/Checkbox/Checkbox';
 import style from './LoginForm.module.css'
+import {NavLink} from 'react-router-dom';
+import {RECOVER_PASSWORD, REGISTRATION} from '../../../../../n1-main/m1-ui/routes/Routes';
+import styles from '../../../../../n1-main/m1-ui/header/Header.module.css';
 
 export const LoginForm = () => {
     return (
-        <div>
-            <Input onChange={() => console.log('mailInput')} value={'mailInput'}/>
-            <Input onChange={() => console.log('passvordInput')} value={'passvordInput'}/>
+        <div className={style.loginBlock}>
+            <div className={style.inputBlock}>
+                <Input onChange={() => console.log('mailInput')} value={'mailInput'}/>
+            </div>
+            <div className={style.inputBlock}>
+                <Input onChange={() => console.log('passwordInput')} value={'passwordInput'}/>
+            </div>
             <div className={style.checkboxBlock}>
                 <Checkbox checkboxValue={true} changeValue={() => console.log('checkBoxTrue')}/>
-                remember me
-            </div>
-            <div>
-                fogot
+                <span>remember me</span>
             </div>
             <Button click={() => console.log('clickButton')} value={'login'}/>
-            <div>
-                register
+            <div className={style.link}>
+                <NavLink to={RECOVER_PASSWORD} activeClassName={styles.activeLick}>fogot fassword?</NavLink>
+            </div>
+            <div className={style.link}>
+                <NavLink to={REGISTRATION} activeClassName={styles.activeLick}>register</NavLink>
             </div>
         </div>
     )
