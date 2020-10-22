b5a5faae46f81e5d034d66f081a6a9d038110bd9
Karter13 <KarMiH1975@mail.ru>
2020-10-22 22:08:56 +0300

    change URL

--- src/n0-config/config.ts
+++ src/n0-config/config.ts
@@ -1,2 +1,2 @@
-export const BACK_URL = 'http://localhost:7542/2.0/';//for developers
-// export const BACK_URL = 'https://github.com/IgnatZakalinsky/cards-nya-back-2-0';//alpha release
+// export const BACK_URL = 'http://localhost:7542/2.0/';//for developers
+export const BACK_URL = 'https://github.com/IgnatZakalinsky/cards-nya-back-2-0';//alpha release
