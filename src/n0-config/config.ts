b19d8cd37ea0be0c7718b0a956dd6e294d1e2a1f
Karter13 <KarMiH1975@mail.ru>
2020-10-22 22:20:47 +0300

    change URL

--- src/n0-config/config.ts
+++ src/n0-config/config.ts
@@ -1,2 +1,3 @@
 // export const BACK_URL = 'http://localhost:7542/2.0/';//for developers
-export const BACK_URL = 'https://github.com/IgnatZakalinsky/cards-nya-back-2-0/';//alpha release
+// export const BACK_URL = 'https://github.com/IgnatZakalinsky/cards-nya-back-2-0/';//alpha release
+export const BACK_URL = 'https://neko-back.herokuapp.com/2.0';//alpha release
