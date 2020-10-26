0b5a3b2e5464c746ebf4877a45a90842c336cefd
Karter13 <KarMiH1975@mail.ru>
2020-10-26 22:34:53 +0300

    test commit

--- src/n2-faetures/f2-cardPacks/c1-packs/p1-ui/Packs1.tsx
+++ src/n2-faetures/f2-cardPacks/c1-packs/p1-ui/Packs1.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MaterialTable from 'material-table';
 
 export const SimpleAction = () => {
+
     return (
         <MaterialTable
             title="CardPacs"
@@ -12,10 +13,10 @@ export const SimpleAction = () => {
                 {title: 'Url', field: 'url', type: 'numeric'},
             ]}
             data={[
-                {name: 'Maikl', cardsCount: 20, updated: '10-26T10:19', url: 'https://github.io/smart-cards'},
-                {name: 'Anna', cardsCount: 30, updated: '10-26T10:19', url: 'https://github.io/smart-cards'},
-                {name: 'Alex', cardsCount: 5, updated: '10-26T10:19', url: 'https://github.io/smart-cards'},
-                {name: 'Masha', cardsCount: 10, updated: '10-26T10:19', url: 'https://github.io/smart-cards'},
+                {name: 'Maikl', cardsCount: 20, updated: '10-10T10:19', url: 'https://github.io/smart-cards'},
+                {name: 'Anna', cardsCount: 30, updated: '10-15T10:19', url: 'https://github.io/smart-cards'},
+                {name: 'Alex', cardsCount: 5, updated: '10-20T10:19', url: 'https://github.io/smart-cards'},
+                {name: 'Masha', cardsCount: 10, updated: '10-25T10:19', url: 'https://github.io/smart-cards'},
             ]}
             actions={[
                 {
@@ -40,10 +41,10 @@ export const SimpleAction = () => {
                 headerStyle: {
                     backgroundColor: '#01579b',
                     color: '#FFF',
-                    width: '90%'
                 },
                 actionsColumnIndex: -1
             }}
+            style={{width: '80%', margin: '0 auto'}}
         />
     )
 }
